c14e494e1b57ec06931a166147326154888b1fb6
ajit1201 <ajitt7381@gmail.com>
2023-06-27 22:45:03 +0530

    after mx stock profit

---
+++ ArrayCC/stock.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class stock {
+    public static void main(String[] args) {
+        int arr[] = { 3, 1, 5, 6, 3, 4 };
+        System.out.println(maxProfit(arr));
+    }
+
+    public static int maxProfit(int arr[]) {
+        int maxPrice = Integer.MIN_VALUE;
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] > maxPrice)maxPrice = arr[i]; minPrice = Integer.MAX_VALUE;
+            if(arr[i] < minPrice)
+            {
+                minPrice = arr[i];
+                maxProfit = (maxProfit < maxPrice - minPrice)?(maxPrice - minPrice):maxProfit;
+            }
+        }
+        return maxProfit;
+    }
+    /*
+     * 1. buy - sell
+     * 2. [7, 1, 8, 3, 6, 4]
+     * 3. max price = -INF;
+     * 3. min price = +INF;
+     * if max < a[i] --> update max, set min = 0;
+     * if min > a[i] ---> update min, calculate max profit
+     * 
+     * 4. profit
+     * 
+     */
+}
